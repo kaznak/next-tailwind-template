28579de66d3ee4d20ff68d7051c6d8ae9dd4581c
kaznak <kaznak.at.work@gmail.com>
2021-12-05 01:32:51 +0900

    lib/withSession.ts: add

---
+++ lib/withSession.ts
@@ -0,0 +1,31 @@
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next'
+import {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextApiHandler,
+} from 'next'
+
+import { sessionOptions } from 'configs/app'
+
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      email: string
+    }
+  }
+}
+
+export function withSessionRoute(handler: NextApiHandler) {
+  return withIronSessionApiRoute(handler, sessionOptions)
+}
+
+// Theses types are compatible with InferGetStaticPropsType https://nextjs.org/docs/basic-features/data-fetching#typescript-use-getstaticprops
+export function withSessionSsr<
+  P extends { [key: string]: unknown } = { [key: string]: unknown }
+>(
+  handler: (
+    context: GetServerSidePropsContext
+  ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>
+) {
+  return withIronSessionSsr(handler, sessionOptions)
+}
