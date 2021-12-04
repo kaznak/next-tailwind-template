6dffd81f7d59e6f9c5016f9f926835a4b34287d2
kaznak <kaznak.at.work@gmail.com>
2021-12-04 15:08:54 +0900

    pages/index.tsx: change the alert message

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export function Home() {
         <button
           className="my-3 px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
           onClick={() => {
-            window.alert('With typescript and Jest')
+            window.alert('With typescript and tailwindcss')
           }}
         >
           Test Button
