675260be92c7249aac8b528e5a48eb1102c3a196
kaznak <kaznak.at.work@gmail.com>
2021-12-07 01:03:36 +0900

    test/testUtils/TestWrapper.tsx: add

---
+++ test/testUtils/TestWrapper.tsx
@@ -0,0 +1,16 @@
+// import { ThemeProvider } from "my-ui-lib"
+// import { TranslationProvider } from "my-i18n-lib"
+// import defaultStrings from "i18n/en-x-default"
+
+export const TestWrapper = ({ children }) => {
+  return children
+  // return (
+  //   <ThemeProvider theme="light">
+  //     <TranslationProvider messages={defaultStrings}>
+  //       {children}
+  //     </TranslationProvider>
+  //   </ThemeProvider>
+  // )
+}
+
+export default TestWrapper
