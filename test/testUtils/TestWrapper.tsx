713ab52f61edc329b8d05b8a05835927bc1c3d28
kaznak <kaznak.at.work@gmail.com>
2021-12-07 01:05:44 +0900

    test/testUtils/TestWrapper.tsx: add SWRConfig

--- test/testUtils/TestWrapper.tsx
+++ test/testUtils/TestWrapper.tsx
@@ -1,9 +1,24 @@
+import { SWRConfig } from 'swr'
+import fetchJson from 'lib/fetchJson'
 // import { ThemeProvider } from "my-ui-lib"
 // import { TranslationProvider } from "my-i18n-lib"
 // import defaultStrings from "i18n/en-x-default"
 
 export const TestWrapper = ({ children }) => {
-  return children
+  return (
+    <SWRConfig
+      value={{
+        fetcher: fetchJson,
+        onError: (err) => {
+          console.error(err)
+        },
+        provider: () => new Map(),
+        dedupingInterval: 0,
+      }}
+    >
+      {children}
+    </SWRConfig>
+  )
   // return (
   //   <ThemeProvider theme="light">
   //     <TranslationProvider messages={defaultStrings}>
