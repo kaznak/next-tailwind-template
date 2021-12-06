fec2ad002f7663d20f085901e6bd1d192afd4f82
kaznak <kaznak.at.work@gmail.com>
2021-12-06 15:55:52 +0900

    pages/api/hello.ts: export response type

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { ApiRoute } from 'lib/apiRoute'
 
-type HelloApiResponse = {
+export type HelloApiResponse = {
   name: string
 }
 
