e6742384532bdfd152544f5d34a394713d6d0d88
kaznak <kaznak.at.work@gmail.com>
2021-12-07 01:53:49 +0900

    pages/api/auth.ts: use imported types

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -9,36 +9,36 @@ const loginRoute = new ApiRoute()
 
 loginRoute.handlers['GET'] = (req, res) => {
   // get sign-in state
-  const { user } = req.session
-  res.send({ ok: true, user })
+  const { auth } = req.session
+  res.send({ ok: true, auth })
 }
 
 loginRoute.handlers['POST'] = async (req, res) => {
   // sign-in
   const { email } = req.body
   // !TODO! sign in operations
-  const user = { email }
-  req.session.user = user
+  const auth = { email }
+  req.session.auth = auth
   await req.session.save()
-  res.send({ ok: true, user })
+  res.send({ ok: true, auth })
 }
 
 loginRoute.handlers['PUT'] = async (req, res) => {
   // sign-up
   const { email } = req.body
   // !TODO! sign up operations
-  const user = { email }
-  req.session.user = user
+  const auth = { email }
+  req.session.auth = auth
   await req.session.save()
-  res.send({ ok: true, user })
+  res.send({ ok: true, auth })
 }
 
 loginRoute.handlers['DELETE'] = async (req, res) => {
   // sign-out
-  req.session.user = undefined
+  req.session.auth = undefined
   await req.session.save()
-  const { user } = req.session
-  res.send({ ok: true, user })
+  const { auth } = req.session
+  res.send({ ok: true, auth })
 }
 
 export default withSessionRoute(loginRoute.handler)
