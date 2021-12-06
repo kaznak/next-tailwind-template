453f59e18d2cee5f2fe245ded08b70989f090020
kaznak <kaznak.at.work@gmail.com>
2021-12-06 11:58:35 +0900

    lib/apiRoute.ts: update

--- lib/apiRoute.ts
+++ lib/apiRoute.ts
@@ -23,17 +23,29 @@ export class ApiRoute<T = any> {
   defaultHandler: NextApiHandler<T>
   defaultErrorHandler: NextApiExtendedHandler<T>
   get handler(): NextApiHandler<T> {
-    return (req, res) => {
-      const { method } = req
-      const handler = this.handlers[method]
-      try {
+    if (this.defaultErrorHandler) {
+      return (req, res) => {
+        const { method } = req
+        const handler = this.handlers[method]
+        try {
+          if (handler) {
+            return handler(req, res)
+          } else {
+            return this.defaultHandler(req, res)
+          }
+        } catch (error) {
+          return this.defaultErrorHandler(req, res, error)
+        }
+      }
+    } else {
+      return (req, res) => {
+        const { method } = req
+        const handler = this.handlers[method]
         if (handler) {
           return handler(req, res)
         } else {
           return this.defaultHandler(req, res)
         }
-      } catch (error) {
-        return this.defaultErrorHandler(req, res, error)
       }
     }
   }
@@ -42,16 +54,21 @@ export class ApiRoute<T = any> {
   }
 
   constructor(
-    handlers: HandlersMap<T> = {},
+    handlers: HandlersMap<T> = undefined,
     defaultHandler = undefined,
     defaultErrorHandler = undefined
   ) {
-    this.handlers = handlers
+    this.handlers = handlers || { OPTIONS: this.methodOptionHandler }
     this.defaultHandler = defaultHandler || this.status405handler
     this.defaultErrorHandler = defaultErrorHandler || this.status500handler
   }
 
   /* default handlers */
+  protected readonly methodOptionHandler: NextApiHandler<T> = (_, res) => {
+    res.setHeader('Allow', this.allowMethods)
+    res.status(204).end()
+  }
+
   protected readonly status405handler: NextApiHandler<T> = (req, res) => {
     res.setHeader('Allow', this.allowMethods)
     res.status(405).end(`Method ${req.method} Not Allowed`)
