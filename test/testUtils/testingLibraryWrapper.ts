ff537ba53ecf3d568b5e8863f951011960694d90
kaznak <kaznak.at.work@gmail.com>
2021-12-07 01:04:04 +0900

    test/testUtils/testingLibraryWrapper.ts: refactor

--- test/testUtils/testingLibraryWrapper.ts
+++ test/testUtils/testingLibraryWrapper.ts
@@ -1,21 +1,8 @@
 import { render } from '@testing-library/react'
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
-
-const Providers = ({ children }) => {
-  return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
-}
+import TestWrapper from './TestWrapper'
 
 const customRender = (ui, options = {}) =>
-  render(ui, { wrapper: Providers, ...options })
+  render(ui, { wrapper: TestWrapper, ...options })
 
 // re-export everything
 export * from '@testing-library/react'
