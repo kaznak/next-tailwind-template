7870bbb118c47b36c3ab77bcc92f64aed2741aeb
kaznak <kaznak.at.work@gmail.com>
2021-12-06 13:59:10 +0900

    pages/api/auth.ts: add

---
+++ pages/api/auth.ts
@@ -0,0 +1,40 @@
+import { withSessionRoute } from 'lib/withSession'
+import { ApiRoute } from 'lib/apiRoute'
+
+const loginRoute = new ApiRoute()
+
+loginRoute.handlers['GET'] = (req, res) => {
+  // get sign-in state
+  const { user } = req.session
+  res.send({ ok: true, user })
+}
+
+loginRoute.handlers['POST'] = async (req, res) => {
+  // sign-in
+  const { email } = req.body
+  // !TODO! sign in operations
+  const user = { email }
+  req.session.user = user
+  await req.session.save()
+  res.send({ ok: true, user })
+}
+
+loginRoute.handlers['PUT'] = async (req, res) => {
+  // sign-up
+  const { email } = req.body
+  // !TODO! sign up operations
+  const user = { email }
+  req.session.user = user
+  await req.session.save()
+  res.send({ ok: true, user })
+}
+
+loginRoute.handlers['DELETE'] = async (req, res) => {
+  // sign-out
+  req.session.user = undefined
+  await req.session.save()
+  const { user } = req.session
+  res.send({ ok: true, user })
+}
+
+export default withSessionRoute(loginRoute.handler)
