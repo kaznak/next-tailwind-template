d8050699821370d80e949316827144894d754de9
kaznak <kaznak.at.work@gmail.com>
2021-12-12 05:33:36 +0900

    pages/profile-sg.tsx: update

--- pages/profile-sg.tsx
+++ pages/profile-sg.tsx
@@ -1,33 +1,14 @@
 import useAuth from 'lib/useAuth'
-import { useState } from 'react'
-import { fetchJson, FetchError } from 'lib/fetchJson'
 
 export default function SgProfile() {
-  const { auth, mutateAuth } = useAuth({
+  const { errorMessage, auth, signOutHandler } = useAuth({
     redirectTo: '/signin',
   })
-  const [errorMessage, setErrorMessage] = useState('')
-  const signOut = async () => {
-    try {
-      mutateAuth(
-        await fetchJson('/api/auth', {
-          method: 'DELETE',
-          headers: { 'Content-Type': 'application/json' },
-        })
-      )
-    } catch (error) {
-      if (error instanceof FetchError) {
-        setErrorMessage(error.data.message)
-      } else {
-        console.error('An unexpected error happened:', error)
-      }
-    }
-  }
 
   return (
     <div className="container">
       <p>{JSON.stringify(auth)}</p>
-      <button onClick={signOut}>Sign Out</button>
+      <button onClick={signOutHandler}>Sign Out</button>
       {errorMessage && <p className="error">{errorMessage}</p>}
     </div>
   )
